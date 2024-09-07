8565274353ff3fa5c695d70b7216a624d0f9fab7
DullSaurcer <kbatbouta66@gmail.com>
2024-09-07 14:27:05 -0700

    Update relabler.py

--- src/relabler.py
+++ src/relabler.py
@@ -1,21 +1,15 @@
 import numpy as np
 
 
-def create_model(entity_types, embedding_model, llm_call_function, name="default"):
+def create_model(entity_types, embedding_model, name="default"):
     clusters = {}
-    generated_examples = []
     for cat in entity_types:
-        examples = [
-            (s['name'], llm_call_function(f"Provide random very very short example text about the following topic '{s['name']}'"))
-            for s in cat["subcategories"]
-        ]
-        generated_examples.append(examples)
         vectors = embedding_model.encode(sentences=[f"{cat['name']}: {cat['description']}"] + [
             f"{s['name']}: {s['description']}" for s in cat["subcategories"]
-        ] + [e[1] for e in examples])
-        clusters[cat['name']] = np.mean(vectors, axis=0)
+        ])
+        vectors = [list(v.astype(np.float64)) for v in vectors]
+        clusters[cat['name']] = dict(zip([s['name'] for s in cat["subcategories"]], vectors))
     return {
         "name": name,
-        "clusters": list(clusters),
-        "generated_examples": generated_examples
+        "clusters": clusters,
     }
